74dfbd69c289a42944277ff8215cbd393c344d47
Koen Wösten <koen.wosten@gmail.com>
2023-03-27 14:04:49 +0200

    give buttons a right and wrong styling

---
+++ modules/right-wrong/right-wrong.js
@@ -0,0 +1,23 @@
+
+var current_right = "left-button";
+
+const buttons = document.querySelectorAll('.btn');
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', addClass, false);
+}
+
+function addClass(event) {
+    let rightwrong = "wrong"
+    if (event.currentTarget.id == current_right) {
+        rightwrong = "right"
+    }
+    event.currentTarget.classList.add(rightwrong);
+
+    if (event.currentTarget.id == current_right) {
+        event.currentTarget.querySelector('span').textContent = "CORRECT"
+    } else {
+        event.currentTarget.querySelector('span').textContent = "WRONG"
+    }
+}
+
